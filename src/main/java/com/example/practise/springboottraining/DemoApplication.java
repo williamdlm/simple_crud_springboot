fa5df54c649e93e3d01d0eef31e792b14febe412
William David de Lima Mota <williammota.david@gmail.com>
2023-05-03 09:12:48 -0300

    Added swagger to the project

--- src/main/java/com/example/practise/springboottraining/DemoApplication.java
+++ src/main/java/com/example/practise/springboottraining/DemoApplication.java
@@ -3,8 +3,10 @@ package com.example.practise.springboottraining;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class DemoApplication  implements CommandLineRunner {
 
 	public static void main(String[] args) {
